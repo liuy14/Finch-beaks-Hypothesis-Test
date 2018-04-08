2ea9bd0d031e94d9955b4d2140ba05e7d0636bfb
liuy14 <33042463+liuy14@users.noreply.github.com>
2018-04-07 18:58:03 -0700

    Add files via upload

---
+++ Finch beaks hypothesis test/finch_beaks.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  3 14:07:38 2018
+
+@author: Yang
+"""
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('datasets/beak_depth_scandens.csv')
